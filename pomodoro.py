5b3475aa148fdf3b701df3b13505d3cb9621f0b4
Victor Klimov <4klimov@gmail.com>
2016-12-23 20:11:08 +0300

    Change place to store sound file

--- pomodoro.py
+++ pomodoro.py
@@ -18,7 +18,7 @@ print "Timer is set: %s" % time.asctime()
 time.sleep(seconds)
 pygame.init()
 # print(pygame.mixer_music.get_volume())
-pygame.mixer_music.load("/opt/my_pomodoro/ingame_07.ogg")  # "/usr/share/gnome-pomodoro/sounds/birds.ogg"
+pygame.mixer_music.load("/opt/my_shell_utilities/ingame_07.ogg")  # "/usr/share/gnome-pomodoro/sounds/birds.ogg"
 pygame.mixer_music.set_volume(0.5)
 pygame.mixer_music.play()
 # print "Take a rest, dear Victor ;)"
