0e334f6af00d74d1ce5130cd6ee6b0078b4c37b2
Victor Klimov <4klimov@gmail.com>
2017-01-10 12:14:50 +0300

    Change .bashrc and pomodoro.py

--- pomodoro.py
+++ pomodoro.py
@@ -1,26 +1,30 @@
 #!/usr/bin/env python
+#!/usr/bin/env python
 """
 Pomodoro timer
 
 Created on Sun Sep 11 13:30:00
 @author: Victor Klimov
 """
-import sys
-import time
 import pygame
-# try:
-#     minutes = float(sys.argv[1])
-# except:
-minutes = 10.0
+import random
+import time
+
+
+tracks = [
+('Depeche_Mode_-_Everything_Counts_Reprise_.ogg', .8, 17.25)
+]
+
+minutes = .1
 seconds = minutes * 60
 print "Timer is set: %s" % time.asctime()
-# start = time.time()
 time.sleep(seconds)
+
+track = random.choice(tracks)
+
 pygame.init()
-# print(pygame.mixer_music.get_volume())
-pygame.mixer_music.load("/opt/my_shell_utilities/ingame_07.ogg")  # "/usr/share/gnome-pomodoro/sounds/birds.ogg"
-pygame.mixer_music.set_volume(0.5)
-pygame.mixer_music.play()
-# print "Take a rest, dear Victor ;)"
-# time.sleep(60)
-time.sleep(11.155)
+song = pygame.mixer.Sound(track[0])
+song.set_volume(track[1])
+song.play()
+time.sleep(track[2])
+pygame.quit()
