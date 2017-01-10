563eb33464f84d13a117010e4e2529dedabceb07
Victor Klimov <4klimov@gmail.com>
2017-01-10 13:54:42 +0300

    Add notification

--- pomodoro.py
+++ pomodoro.py
@@ -6,6 +6,7 @@ Pomodoro timer
 Created on Sun Sep 11 13:30:00
 @author: Victor Klimov
 """
+import notify2
 import pygame
 import random
 import time
@@ -15,16 +16,29 @@ tracks = [
 ('Depeche_Mode_-_Everything_Counts_Reprise_.ogg', .8, 17.25)
 ]
 
-minutes = .1
+minutes = 25.0
 seconds = minutes * 60
 print "Timer is set: %s" % time.asctime()
 time.sleep(seconds)
 
 track = random.choice(tracks)
 
+notify2.init("markup")
+n = notify2.Notification("Rest",
+  '''
+  <b>Take a rest dear <i>Victor</i>!</b>
+  '''
+)
+
 pygame.init()
 song = pygame.mixer.Sound(track[0])
 song.set_volume(track[1])
 song.play()
+n.show()
+
 time.sleep(track[2])
 pygame.quit()
+
+
+
+
